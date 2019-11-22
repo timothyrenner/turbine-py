9ada9ce229ae1cef614bfa1be85fae808ee1faa0
Tim Renner <renner.timothy@gmail.com>
2019-11-22 07:51:52 -0600

    Be more explicit about private members.

--- turbine/turbine.py
+++ turbine/turbine.py
@@ -7,22 +7,22 @@ from itertools import repeat
 class Turbine:
     def __init__(self):
         # Map the channel aliases to a channel.
-        self.channels = {}
-        self.entry_point = None
-        self.tasks = []
+        self._channels = {}
+        self._entry_point = None
+        self._tasks = []
 
     def source(self, outbound_name: str) -> Callable:
         # Add the outbound channel to the channel map.
-        if outbound_name not in self.channels:
-            self.channels[outbound_name] = Queue()
+        if outbound_name not in self._channels:
+            self._channels[outbound_name] = Queue()
 
         def decorator(f: Callable) -> Callable:
             @wraps(f)
             async def wrapper(*args, **kwargs):
                 value = f(*args, **kwargs)
-                await self.channels[outbound_name].put(value)
+                await self._channels[outbound_name].put(value)
 
-            self.entry_point = wrapper
+            self._entry_point = wrapper
             return wrapper
 
         return decorator
@@ -32,25 +32,25 @@ class Turbine:
     ) -> Callable:
         # Add the outbound channels to the channel map.
         for outbound_name in outbound_names:
-            if outbound_name not in self.channels:
-                self.channels[outbound_name] = Queue()
+            if outbound_name not in self._channels:
+                self._channels[outbound_name] = Queue()
 
         def decorator(f: Callable) -> Callable:
             # Create the async task that applies the function.
             async def task():
                 while True:
-                    input_value = await self.channels[inbound_name].get()
+                    input_value = await self._channels[inbound_name].get()
                     # Call the function on the inputs ...
                     output = f(input_value)
                     # ... and copy the outputs to each of the outbound
                     # channels.
                     for output, channel in zip(repeat(output), outbound_names):
-                        await self.channels[channel].put(output)
-                    self.channels[inbound_name].task_done()
+                        await self._channels[channel].put(output)
+                    self._channels[inbound_name].task_done()
 
             # Create all of the tasks.
             for _ in range(num_tasks):
-                self.tasks.append(task)
+                self._tasks.append(task)
             return f
 
         return decorator
@@ -77,19 +77,19 @@ class Turbine:
         def decorator(f: Callable) -> Callable:
             async def task():
                 while True:
-                    value = await self.channels[inbound_name].get()
+                    value = await self._channels[inbound_name].get()
                     f(value)
-                    self.channels[inbound_name].task_done()
+                    self._channels[inbound_name].task_done()
 
-            self.tasks.append(task)
+            self._tasks.append(task)
 
         return decorator
 
     async def _run_tasks(self, seq: Iterable) -> None:
-        running_tasks = [ensure_future(t()) for t in self.tasks]
+        running_tasks = [ensure_future(t()) for t in self._tasks]
         for s in seq:
-            await self.entry_point(s)
-        for _, q in self.channels.items():
+            await self._entry_point(s)
+        for _, q in self._channels.items():
             await q.join()
         for t in running_tasks:
             t.cancel()
